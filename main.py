25192798816c2ed37cde7951e21f30626837252a
Shivam Bansal <shivam5992@gmail.com>
2014-06-13 23:23:19 +0530

    more variables

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-import sys, codecs
+import sys, codecs, re
 from TwitterAPI import TwitterAPI, TwitterRestPager
 from tweetprocess import *
 from pprint import pprint
@@ -28,6 +28,7 @@ def cleaned(text):
 
 def track_words(text):
     for word in text.split():
+        word_count_les.append(word)
         if word.lower() in positives:
             pos_tracked.append(word)
         elif word.lower() in negatives:
@@ -36,7 +37,10 @@ def track_words(text):
 def flat(lis):
     return list(set(lis))
 
-def background_thread(pager):
+def roundoff(data):
+  return "%.2f" % data
+
+def background_thread(r):
     tweet_length = 0
     tweet_count = 0
     hashtags_count = 0
@@ -44,10 +48,9 @@ def background_thread(pager):
     mentions_count = 0
     negsentiment = 0
     possentiment = 0
+    retweets = 0
 
-    
-    for item in pager.get_iterator():
-
+    for item in r:
         tweet_text = item['text'] if 'text' in item else item
         if type(tweet_text) != dict:
           tweet_text = tweet_text.encode("utf-8").decode("ascii","ignore")
@@ -73,7 +76,7 @@ def background_thread(pager):
           if item['user']['name'] not in user_tracked:
               user_tracked.append(item['user']['name'])
           user_count = len(user_tracked)
-
+          word_count = len(word_count_les)
                   
           blob = TextBlob(cleaned_tweet)
           for sentence in blob.sentences:
@@ -89,16 +92,32 @@ def background_thread(pager):
           possentimentres = "%.2f" % float(possentiment)
           negsentimentres = "%.2f" % float(negsentiment)  
 
+          S = tweet_count
+          L = tweet_length
+          CLI = 0.058 * L - 0.296 * S - 15.8
 
+          CLI = "%.2f" % CLI
 
+          chrs = tweet_length
+          wds = word_count
+          snts = tweet_count
+          ARI = 4.71 * ( float(chrs)/wds ) + 0.5 * ( float(wds)/snts ) - 21.43
+
+          ARI = "%.2f" % ARI
+
+          optimism =  roundoff(float(pos_score)/word_count)
+          pessimism = roundoff(float(neg_score)/word_count)
+
+          if item['retweeted'] == True:
+            retweets += 1
 
           tweetify = {
            "original_tweet": tweet_text,
            "cleaned_tweet": cleaned_tweet,
            "positive_words": flat(pos_tracked),
            "negative_words": flat(neg_tracked),
-           "total_positive_words": pos_score,
-           "total_negative_words": neg_score,
+           "optimism": optimism,
+           "pessimism": pessimism,
            "tweet_count": tweet_count,
            "average_tweet_length": avg_tweet_length,
            "mentions": flat(mentions_tracker),
@@ -109,7 +128,11 @@ def background_thread(pager):
            "user_count": user_count,
            "sentiment_polarity":sentiment_polarity,
            "positivity": possentimentres,
-           "negativity": negsentimentres
+           "negativity": negsentimentres,
+           "CLI1":CLI,
+           "ARI1":ARI,
+           "word_count":word_count,
+           "retweets":retweets
           }
 
           json_encoded = json.dumps(tweetify)
@@ -124,7 +147,7 @@ def background_thread(pager):
 def index():
     if request.method == 'POST':
       tosearch = "#" + request.form['ht']
-      pager = TwitterRestPager(api, 'search/tweets', {'q': tosearch, 'count': 1})
+      r = api.request('statuses/filter', {'track': tosearch})
 
       for thread in enumerate():
         if thread.isAlive():
@@ -133,7 +156,7 @@ def index():
           except:
             print(str(thread.getName()) + ' could not be terminated')
 
-      p = Thread(target=background_thread, args = (pager,))
+      p = Thread(target=background_thread, args = (r,))
       p.start()
       
       return render_template('index.html', tag = tosearch, home=False)
@@ -164,6 +187,7 @@ if __name__ == '__main__':
     pos_tracked, neg_tracked = [], []
     hashtags_tracker, mentions_tracker = [], []
     user_tracked = []
+    word_count_les = []
     
     socketio.run(app)
 
---
+++ try.py
@@ -0,0 +1,31 @@
+import sys, codecs, re
+from textblob import TextBlob
+
+from TwitterAPI import TwitterAPI, TwitterRestPager
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+data = open("tokens.txt").read().split("\n")
+consumer_key = data[0]
+consumer_secret = data[1]
+access_token_key = data[2]
+access_token_secret = data[3]
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+pager = TwitterRestPager(api, 'search/tweets', {'q': 'modi', 'count': 1})
+
+r = api.request('statuses/filter', {'track': 'modi'})
+
+def lexical_density(text):
+	consider = ['JJ', 'JJR', 'JJS', 'NN', 'NNP', 'NNS', 'NNPS', 'RB', 'RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ']
+	blob = TextBlob(text)
+	nlex = 0
+	n = len(blob.words)
+	for tags in blob.tags:
+		if str(tags[1]) in consider:
+			nlex += 1
+	ld = (float(nlex)/n) * 100
+	print "%.2f" % ld
+
+print lexical_density("hello modi ji you are awesome")
+
+# for item in r:
+# 	wiki = TextBlob(item['text'])
+# 	print wiki.tags
